d062790c1862374b5971da2d13c2084a327295e1
Sam Philip <seivadmas@gmail.com>
2014-04-11 12:06:12 -0300

    added Ucl field to form

--- app/models/ucl.rb
+++ app/models/ucl.rb
@@ -1,4 +1,8 @@
 class Ucl < ActiveRecord::Base
   has_and_belongs_to_many :workers
   has_many :instruments, dependent: :destroy
+
+  def self.options_for_select
+    Ucl.all.map{|s| [s.code.to_s + " - " + s.name, s.id] }
+  end
 end
