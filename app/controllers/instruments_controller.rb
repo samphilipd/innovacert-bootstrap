d7aebeb416e9a9f7fd1faa35df06ba3ab3d7f520
Sam Philip <seivadmas@gmail.com>
2014-04-11 12:46:30 -0300

    Tweaked form labels

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -5,15 +5,23 @@ class InstrumentsController < ApplicationController
     @instrument = Instrument.new
   end
 
+  # POST /instruments
   def create
     @instrument = Instrument.new(instrument_params)
+    respond_to do |format|
+      if @instrument.save
+        format.html { redirect_to root_url, notice: "Instrument created successfully!" }
+      else
+        format.html { render action: :new }
+      end
+    end
   end
 
 
   private
   # Never trust parameters from the scary internet, only allow the white list through.
    def instrument_params
-     params.require(:instrument).permit()
+     params.require(:instrument).permit(:name, :ucl_id)
    end
 
 end
