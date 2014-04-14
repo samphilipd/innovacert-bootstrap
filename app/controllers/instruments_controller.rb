d22f1a8261fe765c42bafe3d64261e450d446325
Sam Philip <seivadmas@gmail.com>
2014-04-14 15:20:41 -0300

    Show instrument now displays questions

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -49,7 +49,7 @@ class InstrumentsController < ApplicationController
   private
   # Never trust parameters from the scary internet, only allow the white list through.
   def instrument_params
-    params.require(:instrument).permit(:name, :ucl_id, observation_sections_attributes: [:id, :name, :_destroy])
+    params.require(:instrument).permit(:name, :ucl_id, observation_sections_attributes: [:id, :name, :_destroy, observation_questions_attributes: [:id, :content, :_destroy]])
   end
 
   def set_instrument
