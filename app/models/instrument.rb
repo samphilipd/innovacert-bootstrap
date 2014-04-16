2ec1fdfdc697db955286c9a8ee4f314e4be56d47
Sam Philip <seivadmas@gmail.com>
2014-04-16 09:25:07 -0300

    Added UCL presence validation to Instrument

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,6 +2,7 @@ class Instrument < ActiveRecord::Base
   belongs_to :ucl
   has_many :observation_sections, dependent: :destroy
   validates :name, presence: true
+  validates :ucl, presence: true
 
   accepts_nested_attributes_for :observation_sections, reject_if: lambda {|os| os[:name].blank?}, allow_destroy: true
 
--- app/models/ucl.rb
+++ app/models/ucl.rb
@@ -1,6 +1,6 @@
 class Ucl < ActiveRecord::Base
   has_and_belongs_to_many :workers
-  has_many :instruments, dependent: :destroy
+  has_many :instruments, dependent: :destroy, inverse_of: :ucl
 
   def self.options_for_select
     Ucl.all.map{|s| [s.human_readable_name, s.id] }
