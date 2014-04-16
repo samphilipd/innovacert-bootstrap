7911fea769fce34e1c1703d52088e2c699ce530e
Sam Philip <seivadmas@gmail.com>
2014-04-16 09:53:49 -0300

    Added validations for Ucl model

--- app/models/ucl.rb
+++ app/models/ucl.rb
@@ -2,6 +2,11 @@ class Ucl < ActiveRecord::Base
   has_and_belongs_to_many :workers
   has_many :instruments, dependent: :destroy, inverse_of: :ucl
 
+  ## VALIDATIONS
+  validates :code, presence: true
+  validates :name, presence: true
+  # description is not required for now
+
   def self.options_for_select
     Ucl.all.map{|s| [s.human_readable_name, s.id] }
   end
